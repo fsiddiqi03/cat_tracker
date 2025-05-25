0caf668f7ef5d46b9a4dd1e220f656d7d99b836e
fsiddiqi03 <farissiddiqi@gmail.com>
2025-05-25 15:30:51 -0500

    Adjusted script to wait for motion before sending frames to api

--- main.py
+++ main.py
@@ -1,6 +1,10 @@
 from picamera2 import Picamera2
 import cv2
 import time
+from ultralytics import YOLO
+from api import API
+from config import api_url
+from gpiozero import MotionSensor
 
 
 # Set up the camera with Picam
@@ -11,20 +15,41 @@ picam2.preview_configuration.align()
 picam2.configure("preview")
 picam2.start()
 
-# Give camera time to warm up
-time.sleep(1)
+# Api Setup 
+sender = API(api_url)
+pir = MotionSensor(4)
 
-while True:
-    # Capture an image
-    image = picam2.capture_array()
+try:
+    while True:
 
-    # Display the image in a window
-    cv2.imshow("Live Image", image)
+        
+        
+        if pir.motion_detected:
+            print("Motion detected!")
+           
+            # capture frame from camera
+            frame = picam2.capture_array()
+            _, jpeg = cv2.imencode('.jpg', frame, [cv2.IMWRITE_JPEG_QUALITY, 70])
 
-    # Wait for a key press and check if it's 'q' to exit
-    if cv2.waitKey(1) & 0xFF == ord('q'):
-        break
-# Release the camera and close all OpenCV windows
-picam2.close()
-cv2.destroyAllWindows()
+            timestame = time.time()
+            file_name = f"cat_{timestame}.jpg"
+
+            # call api 
+            response = sender.send_request(jpeg.tobytes(), file_name)
+            print(response)
+            # check if the response is valid
+            if response.status_code == 200:
+                print("Image sent successfully")
+            else:
+                print("Failed to send image")
+
+
+
+except KeyboardInterrupt:
+    print("Exiting...")
+finally:
+    picam2.stop()
+    picam2.close()
+    print("Camera stopped and closed.")
+
 
